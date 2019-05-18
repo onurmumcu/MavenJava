ca0ca323df7bfa1784c7b91685fe67284b9b53fb
onur.mmc@gmail.com <Onur@DESKTOP-7M3VN73>
2019-05-18 12:46:41 +0300

    ilk commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppAndroid() {
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSApp() {
+		System.out.println("IOSApp");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SelenimTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SelenimTest {
+
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+
+		
+	}
+	
+	
+	@Test
+	public void elementsUI() {
+		System.out.println("elementsUI");
+
+	}
+	
+	
+}
